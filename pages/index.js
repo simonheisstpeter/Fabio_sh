83f574d47a7827b51d56dda7b481ac6a2d8d1dba
simonheisstpeter <f.gschweidl@gmail.com>
2021-01-27 23:29:19 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,27 @@ export default function Home({ links }) {
       <Head>
         <title>fabio</title>
         <link rel="icon" href="/carp_streamer.ico" />
+
+        <meta name="Title" content="Fabio Gschweidl"/>
+        <meta name="Author" content="Fabio Gschweidl"/>
+        <meta name="Publisher" content="Fabio Gschweidl"/>
+        <meta name="Copyright" content="Fabio Gschweidl"/>
+        <meta name="Revisit" content="After 5 days"/>
+        <meta name="Keywords" content="Fabio Gschweidl Webdeveloper Webdevelopment Webdesign"/>
+        <meta name="Description" content="Fabio Gschweidl Webdeveloper Webdevelopment Webdesign"/>
+        <meta name="Abstract" content="Fabio Gschweidl Webdeveloper Webdevelopment Webdesign"/>
+        <meta name="page-topic" content="Medien"/>
+        <meta name="page-topic" content="Private Homepage"/>
+        <meta name="audience" content="Erwachsene"/>
+        <meta name="Robots" content="INDEX,FOLLOW"/>
+        <meta name="Language" content="en"/>
+
+        <meta property="og:title" content="Fabio Gschweidl Webdeveloper Webdevelopment Webdesign" />
+        <meta property="og:image" content="/carp_streamer.ico" />
+        <meta property="og:description" content="Coming soon ..." />
+        <meta property="og:type" content="Website" />
+        <meta property="og:site_name" content="Fabio Gschweidl" />
+        <meta property="og:url" content="https://fabio.sh" />
       </Head>
       <div className="container mx-auto pt-32 md:pt-72">
         <div class="px-6 md:px-12 text-6xl md:text-9xl font-bold md:font-bold">
