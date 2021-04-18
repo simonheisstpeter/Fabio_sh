493dc42da60f52b60eefc88a8265d9e3b831ec53
simonheisstpeter <f.gschweidl@gmail.com>
2021-04-18 20:22:07 +0200

    spotify player nealry working

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,14 @@
-export default function About({ data }) {
+import useSWR from 'swr'
 
+export default function About() {
 
+    const { data, error } = useSWR('/api/player', fetch)
+    let text = "This Song plays now: "
+
+    if (error) return <div>failed to load</div>
+    if (!data) return <div>loading...</div>
+
+    if (data.redirected === false ) text = "No music is playing on Spotify";
 
     return (
         <div className="h-full w-full">
@@ -9,10 +17,15 @@ export default function About({ data }) {
         {/*
             bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-green-400
         */}
-        <span className="text-center block">
+        <span className="text-center block mb-20">
             Hier kommt bald <span className="text-md font-bold">ETWAS.... Neues</span>
         </span>
-  
+        
+        <hr />
+        <p className="mt-20 text-md text-center">
+            Here some information about music playing on my Spotify account
+        </p>
+        <span className="block my-4 text-md font-bold text-center">{text}</span>
         </div>
       </div>
     )};
