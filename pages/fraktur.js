7a886030b1d55a171fcc0933b5f27f95faa02622
simonheisstpeter <f.gschweidl@gmail.com>
2021-09-09 20:11:48 +0200

    added small pngs

--- pages/fraktur.js
+++ pages/fraktur.js
@@ -9,41 +9,42 @@ export default function Fraktur() {
 
             <div className="container mx-auto pt-20 md:pt-60">
                 <p className="mx-auto text-center">Hier kommt neues ... </p>
-                <p className="mx-auto text-center mb-52">SVGs laden derzeit nicht immer aber <span className="text-md font-bold">Bald</span> ....</p>
-
-            <div className="px-6 md:px-12 mb-12 h-full grid grid-cols-2 md:grid-cols-5 gap-12 gap-y-24">
-                <Image src={'/f/0.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/1.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/2.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/3.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/4.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/5.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/6.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/7.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/8.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/9.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/10.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/11.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/12.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/13.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/14.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/15.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/16.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/17.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/18.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/19.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/20.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/21.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/22.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/23.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/24.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/25.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/26.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/27.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/28.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/29.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/30.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
-                <Image src={'/f/31.svg'} width={100} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <p className="mx-auto text-center mb-52"><span className="text-md font-bold">Bald</span> ....</p>
+            <div className="">
+            <div className="px-6 md:px-12 mb-12 h-full grid grid-cols-2 sm:grid-cols-4 md:grid-cols-8 xl:grid-cols-12 gap-12 gap-y-24">
+                <Image src={'/f/0.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/1.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/2.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/3.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/4.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/5.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/6.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/7.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/8.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/9.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/10.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/11.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/12.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/13.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/14.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/15.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/16.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/17.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/18.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/19.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/20.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/21.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/22.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/23.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/24.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/25.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/26.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/27.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/28.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/29.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/30.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+                <Image src={'/f/31.png'} width={88} height={150} className="p-4 duration-300 transform hover:scale-105"/>
+            </div>
             </div>
             <div className="p-6 md:p-12 md:pt-60">
                 <p>Email</p>
