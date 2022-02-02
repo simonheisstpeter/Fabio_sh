d85a5cf62279bc54ea28230a295df3bf9282d1f5
simonheisstpeter <f.gschweidl@gmail.com>
2022-02-03 00:11:59 +0100

    Update navigation.js

--- components/navigation.js
+++ components/navigation.js
@@ -18,11 +18,6 @@ export default function Navigation () {
 
   if (!mounted) return null
     
-  const setMobileMenu = (themeSelected) => {
-      console.log(themeSelected)
-        setTheme(themeSelected);
-        toggleMenu();
-    }
       
     return (
         <>
@@ -53,11 +48,11 @@ export default function Navigation () {
                       <Link href="/contact">
                         <a className="mb-20 bg-clip-text text-transparent bg-gradient-to-r from-emerald-600 to-emerald-800">Kontakt</a>
                       </Link>
-                      <button className={theme === 'dark' ? "hover:text-emerald-600" : "hidden"} onClick={() => { toggleMenu(); setTheme('light'); }}>
+                      <button className={theme === 'dark' ? "block text-emerald-400 hover:text-emerald-600" : "hidden"} onClick={() => { toggleMenu(); setTheme('light'); }}>
                             <svg className="mb-6 text-emerald-400 mx-auto h-8 w-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" /></svg>
                         </button>
-                        <button className={theme === 'light' ? "hover:text-emerald-600" : "hidden"} onClick={() => { toggleMenu(); setTheme('dark'); }}>
-                            <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" id="moon" className="mb-6 text-emerald-400 mx-auto h-8 w-8"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"></path></svg>
+                        <button className={theme === 'light' ? "block text-emerald-400 hover:text-emerald-600" : "hidden"} onClick={() => { toggleMenu(); setTheme('dark'); }}>
+                            <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" id="moon" className="mb-6  mx-auto h-8 w-8"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"></path></svg>
                         </button>  
                     </div>
                 </div>
