80086e1f49c784402f101cf250e8c65b8de7e6bc
simonheisstpeter <f.gschweidl@gmail.com>
2022-04-08 22:11:41 +0200

    Revert "Preact!!! for Speeeeed!!!"
    This reverts commit b7e31b493bacb696144d952b8c98d1b80be4250a.

--- next.config.js
+++ next.config.js
@@ -6,15 +6,4 @@ module.exports = {
     locales: ["de", "en"],
     defaultLocale: "de",
   },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
-
-    return config;
-  },
 };
