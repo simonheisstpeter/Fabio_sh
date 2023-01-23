c9cd68113bbd0ecabb89bfd366c9617dd17cac1b
simonheisstpeter <f.gschweidl@gmail.com>
2023-01-24 00:53:01 +0100

    animation fixed

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,12 @@ export default function Home() {
     <Container>
       <div className="h-full w-full">
         <div className="container mx-auto py-32 md:py-60">
-          <div className="p-6 md:p-12 text-6xl md:text-9xl font-bold md:font-bold">
-            <span className="bg-clip-text text-transparent bg-gradient-to-r from-emerald-200 to-emerald-400 animate-fadeIn_2000">
+          <h1 className="p-6 md:p-12 text-6xl md:text-9xl font-bold md:font-bold">
+            <p className="md:w-[315px] bg-clip-text text-transparent bg-gradient-to-r from-emerald-200 to-emerald-600 animate-fadeIn_2000">
               Fabio
-            </span>
-            <br />
-            <span className="animate-fadeIn_2000">Gschweidl</span>
-          </div>
+            </p>
+            <p className="animate-fadeIn_2000">Gschweidl</p>
+          </h1>
         </div>
       </div>
     </Container>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,16 +8,15 @@ module.exports = {
       animation: {
         fadeIn_2000: "fade 2s",
         fadeIn_3000: "fade 3s",
-        moveIn: "move 2s"
       },
       keyframes: {
         fade: {
           "0%": {
-            transform: "translate(0px, 50px)",
+            transform: "translate(-20px, 0px)",
             opacity: 0,
           },
           "100%": {
-            transform: "translate(0px, 0px) scale(1)",
+            transform: "translate(0px, 0px)",
             opacity: 1,
           },
         }
