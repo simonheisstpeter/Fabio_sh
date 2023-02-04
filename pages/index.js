e4b37725dd2cc97c17ae004ae08fda3c493a4921
simonheisstpeter <f.gschweidl@gmail.com>
2023-02-04 21:12:32 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="h-full w-full">
         <div className="container mx-auto py-32 md:py-60">
           <h1 className="p-6 md:p-12 text-6xl md:text-9xl font-bold md:font-bold">
-            <p className="md:w-[315px] text-emerald-500 animate-fadeIn_2000">
+            <p className="md:w-[315px] text-gray-300 dark:text-gray-400 animate-fadeIn_2000">
               Fabio
             </p>
             <p className="animate-fadeIn_2000">Gschweidl</p>
