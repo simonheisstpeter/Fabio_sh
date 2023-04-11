abf1017b96c13d3201e44c43cc0058a543f71f56
simonheisstpeter <f.gschweidl@gmail.com>
2023-04-11 16:08:02 +0200

    Update ja.js

--- locales/ja.js
+++ locales/ja.js
@@ -7,11 +7,11 @@ export default {
   name: "ファビオ",
   lastname: "ゲスウェイデゥ",
 
-  contactTitle: "お問合せフォーム",
-  contactName: "名義",
-  contactEMail: "電子メール",
-  contactMessage: "通信文",
-  contactButton: "レッツゴー",
+  contactTitle: "お問合せフォーム ",
+  contactName: "名義 ",
+  contactEMail: "電子メール ",
+  contactMessage: "通信文 ",
+  contactButton: "レッツゴー ",
 
   custom404Title: "申し訳ありませんが、このページは見つかりませんでした！",
   custom404Button: "バック",
