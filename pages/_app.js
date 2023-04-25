a5959e602544c959869f419e0500c9c0c354befb
simonheisstpeter <f.gschweidl@gmail.com>
2023-04-25 21:28:29 +0200

    SEO cahnged

--- pages/_app.js
+++ pages/_app.js
@@ -17,51 +17,6 @@ export default function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <Head>
-        <title>fabio</title>
-        <link rel="icon" href="/carp_streamer.ico" />
-        <meta name="Title" content="Fabio Gschweidl" />
-        <meta name="Author" content="Fabio Gschweidl" />
-        <meta name="Publisher" content="Fabio Gschweidl" />
-        <meta name="Copyright" content="Fabio Gschweidl" />
-        <meta
-          name="Keywords"
-          content="Fabio Gschweidl Webdeveloper Webdevelopment Webdesign Wireframe Consulting Wireframe Consulting Wireframe Consulting"
-        />
-        <meta
-          name="Description"
-          content="Fabio Gschweidl Webdeveloper Webdevelopment Webdesign Wireframe Consulting Wireframe Consulting Wireframe"
-        />
-        x
-        <meta
-          name="Abstract"
-          content="Fabio Gschweidl Webdeveloper Webdevelopment Webdesign Wireframe Consulting Wireframe Consulting Wireframe"
-        />
-        <meta name="page-topic" content="Medien" />
-        <meta name="audience" content="Erwachsene" />
-        <meta name="Robots" content="INDEX,FOLLOW" />
-        <meta name="Language" content="de" />
-        <meta
-          property="og:title"
-          content="Fabio Gschweidl Webdeveloper Webdevelopment Webdesign Wireframe Consulting Wireframe Consulting Wireframe"
-        />
-        <meta property="og:image" content="/meta_pic.png" />
-        <meta property="og:description" content="Coming soon ..." />
-        <meta property="og:type" content="Website" />
-        <meta property="og:site_name" content="Fabio Gschweidl" />
-        <meta property="og:url" content="https://fabio.sh" />
-        <meta
-          name="theme-color"
-          media="(prefers-color-scheme: light)"
-          content="#f8f8ff"
-        />
-        <meta
-          name="theme-color"
-          media="(prefers-color-scheme: dark)"
-          content="#1d1d1f"
-        />
-      </Head>
-
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
       </ThemeProvider>
