e6326752eaf625ffc647587c2a7b8292c267e69f
Fabio <37487726+simonheisstpeter@users.noreply.github.com>
2023-05-22 15:05:18 +0200

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -1,95 +1,59 @@
 import React, { useEffect, useRef } from "react";
 import * as THREE from "three";
+import { AsciiEffect } from "three/examples/jsm/effects/AsciiEffect";
 
-const Stripes = () => {
+const AsciiAnimation = () => {
   const containerRef = useRef();
 
   useEffect(() => {
-    // Set up the scene and renderer
     const scene = new THREE.Scene();
-    const camera = new THREE.Camera();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 2;
+
     const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    containerRef.current.appendChild(renderer.domElement);
+    const effect = new AsciiEffect(renderer, ' dieDgtale', { invert: true }, { width: window.innerWidth, height: window.innerHeight });
 
-    // Create the stripes material
-    const stripesMaterial = new THREE.ShaderMaterial({
-      uniforms: {
-        color1: { value: new THREE.Color("black") },
-        color2: { value: new THREE.Color("white") },
-        stripeWidth: { value: 0.1 },
-      },
-      vertexShader: `
-                varying vec2 vUv;
-                void main() {
-                  vUv = uv;
-                  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-                }
-              `,
-      fragmentShader: `
-                uniform vec3 color1;
-                uniform vec3 color2;
-                uniform float stripeWidth;
-                varying vec2 vUv;
-                void main() {
-                  float f = mod(vUv.y / stripeWidth, 2.0);
-                  f = step(1.0, f);
-                  gl_FragColor = vec4(mix(color1, color2, f), 1.0);
-                }
-              `,
-    });
+    effect.setSize(window.innerWidth, window.innerHeight);
+    containerRef.current.appendChild(effect.domElement);
 
-    // Create the stripes mesh
-    const stripesGeometry = new THREE.PlaneBufferGeometry(10, 10);
-    const stripes = new THREE.Mesh(stripesGeometry, stripesMaterial);
-    scene.add(stripes);
+    const geometry = new THREE.TubeGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
 
-    // Create the light
-    const light = new THREE.PointLight(0xffffff, 1, 100);
-    light.position.set(5, 5, 5);
-    scene.add(light);
-
-    // Create the deformed balls
-    const ballGeometry = new THREE.IcosahedronGeometry(1, 2);
-    const ballMaterial = new THREE.MeshStandardMaterial({ color: 0xff0000 });
-    const balls = [];
-    for (let i = 0; i < 10; i++) {
-      const ball = new THREE.Mesh(ballGeometry, ballMaterial);
-      ball.position.set(
-        Math.random() * 10 - 5,
-        Math.random() * 10 - 5,
-        Math.random() * 10 - 5
-      );
-      scene.add(ball);
-      balls.push(ball);
-    }
-
-    // Animate the deformed balls movement
-    function animate() {
+    const animate = () => {
       requestAnimationFrame(animate);
 
-      balls.forEach((ball) => {
-        ball.position.x += (Math.random() - 0.5) * 0.1;
-        ball.position.y += (Math.random() - 0.5) * 0.1;
-        ball.position.z += (Math.random() - 0.5) * 0.1;
-        ball.rotation.x += 0.01;
-        ball.rotation.y += 0.02;
-      });
+      cube.rotation.x += 0.02;
+      cube.rotation.y += 0.01;
 
-      renderer.render(scene, camera);
+      effect.render(scene, camera);
     }
+
     animate();
 
-    // Clean up on unmount
+    const onResize = () => {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      effect.setSize(window.innerWidth, window.innerHeight);
+    };
+
+    window.addEventListener('resize', onResize);
+
     return () => {
-      stripesGeometry.dispose();
-      stripesMaterial.dispose();
-      ballGeometry.dispose();
-      ballMaterial.dispose();
+      window.removeEventListener('resize', onResize);
+      containerRef.current.removeChild(effect.domElement);
     };
   }, []);
 
-  return <div ref={containerRef} style={{ width: "100vw", height: "100vh" }} />;
-};
+  return (
+    <>
+    <div className="animate-pulse" ref={containerRef}>
 
-export default Stripes;
+    </div>      <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-start">
+        <h1 className="text-9xl text-gray-100 text-left bg-black hover:bg-emerald-500 duration-200 p-2">dieDigitale</h1>
+      </div></>
+  );
+}
+
+export default AsciiAnimation;
