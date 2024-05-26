495f25634b0a9d5a806dacdbbd701d4fbe48d75b
Fabio Gschweidl <37487726+simonheisstpeter@users.noreply.github.com>
2024-05-26 15:39:34 +0200

    RC

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-  swcMinify: true,
   reactStrictMode: true,
   productionBrowserSourceMaps: true,
   i18n: {
     locales: ["de", "en", "es", "it", "ja", "pt"],
     defaultLocale: "de",
+  },
+  experimental: {
+    reactCompiler: true,
   },
 };
