e3cdcf5af2a5059cf6b2f06949d7d2eb3fa51f84
simonheisstpeter <f.gschweidl@gmail.com>
2026-06-12 14:13:56 +0200

    Update neta.txt.ts

--- src/pages/docs/neta.txt.ts
+++ src/pages/docs/neta.txt.ts
@@ -1,13 +1,7 @@
 import type { APIRoute } from "astro";
-import { readFileSync } from "node:fs";
-import { resolve } from "node:path";
+import content from "../../lib/docs/neta.txt?raw";
 
 export const GET: APIRoute = () => {
-  const content = readFileSync(
-    resolve("src/lib/docs/neta.txt"),
-    "utf-8"
-  );
-
   return new Response(content, {
     headers: {
       "Content-Type": "text/plain; charset=utf-8",
